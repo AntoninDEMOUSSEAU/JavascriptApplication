4b445b39640e53e729d3905cf3dfd0c69ce535ee
anton <antonin.demousseau@gmail.com>
2022-05-17 21:59:55 +0200

    Simplification du code

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,5 @@
+
+
 function getValue(){
     //Fonction permettant d'afficher en temps réelle la valeur de mon input
     realTimeValue();
@@ -10,60 +12,25 @@ function getValue(){
 }
 
 function realTimeValue(){
-    //Je récupere la valeur de mon Element HTML input
-    let valueTV = document.getElementById("input").value;
+    let input = document.getElementById("input").value;
+    let rtv = document.getElementById("reponseTR")
+    rtv.textContent="Voici le rendu en direct :"+input ;
 
-    //Je vais créer un nouvel élement du DOM "p"
-    let newP = document.createElement("p")
-    //Je lui set un id
-    newP.setAttribute("id","reponseTR");
 
-    //Je vérifie si la value contient déjà un caractere.
-    //Si oui, je passe la value à mon nouvelle element P (newP)
-    if (valueTV.length <= 1){
-        newP.textContent=valueTV ;
-        document.body.append(newP)
-    }
-    //Si non je recupere l'ancien element P, le supprime pour en créer un nouveau.
-    else {
-        document.getElementById("reponseTR").remove();
-        newP.textContent=valueTV ;
-        document.body.append(newP)
-    }
 }
 
 function count(){
-
-    let newP2 = document.createElement("p");
-    newP2.setAttribute("id","compteur");
-    newP2.setAttribute("label", "Le nombre de lettre est : ")
-
-    let count = document.getElementById("input").value.length;
-    if (count <= 1){
-        newP2.textContent=count;
-        document.body.append(newP2)
-    } else {
-        document.getElementById("compteur").remove();
-        newP2.textContent=count
-        document.body.append(newP2)
-    }
+    let input = document.getElementById("input").value;
+    let count = document.getElementById("count")
+    count.textContent="Le nombre de caractère est de :" +input.length;
 }
 
 function reverseString(){
+    let input = document.getElementById("input").value;
+    let splitString = input.split("").reverse().join("");
+    let rs = document.getElementById("reverseString")
 
-    let newP1 = document.createElement("p");
-    newP1.setAttribute("id","reverseString");
-
-    let valueRS = document.getElementById("input").value;
-    let splitString = valueRS.split("").reverse().join("");
-
-    if (valueRS.length <= 1){
-        newP1.textContent=splitString
-        document.body.append(newP1)
-    } else {
-        document.getElementById("reverseString").remove()
-        newP1.textContent=splitString
-        document.body.append(newP1)
-    }
+    rs.textContent="La phrase écrite à l'envers est :" + splitString;
 
 }
+
